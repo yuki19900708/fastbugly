15284667895edccebe9076e6ec7c9c7ad4dc0f60
zhengyang <307173204@qq.com>
2025-01-14 11:41:04 +0800

    init

---
+++ Assets/Scenes/Sample.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sample : MonoBehaviour
+{
+    private string appid = "aabe15ec5f";
+    void Start()
+    {
+        //初始化bugly的appid，在bugly创建获取
+#if UNITY_IOS
+        BuglyAgent.InitWithAppId (appid);
+#elif UNITY_ANDROID
+        BuglyAgent.InitWithAppId (appid);
+#endif
+
+        BuglyAgent.ConfigDebugMode(true);
+        BuglyAgent.EnableExceptionHandler();
+        Debug.LogError($"init bugly");
+
+        exception += $"init bugly\n";
+    }
+
+    private GUIStyle style = new GUIStyle();
+    private string exception;
+    private List<int> numbers = new List<int>();
+
+    void TestNullReference()
+    {
+        style.fontSize = 22;
+        GameObject go = null;
+        go.name = "";
+    }
+
+    void TestOutOfRange()
+    {
+        try
+        {
+            numbers[1] = 1;
+        }
+        catch (Exception e)
+        {
+            Debug.LogError($"{e}");
+            exception += $"{e}\n";
+        }
+    }
+
+
+    void OnGUI()
+    {
+        GUILayout.Space(150);
+        GUILayout.BeginHorizontal();
+        if (GUILayout.Button("TestNullReference", GUILayout.Width(200), GUILayout.Height(60)))
+        {
+            TestNullReference();
+        }
+        GUILayout.Space(50);
+        if (GUILayout.Button("OutOfRange", GUILayout.Width(200), GUILayout.Height(60)))
+        {
+            TestOutOfRange();
+        }
+        GUILayout.EndHorizontal();
+        
+        if (!string.IsNullOrEmpty(exception))
+        {
+            GUI.Label(new Rect(0, 100, Screen.width, Screen.height), $"{exception}", style);
+        }
+
+
+    }
+}
